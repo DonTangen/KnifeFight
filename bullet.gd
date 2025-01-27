e0768e0a4e2982ccf9bb2043b2c98de2c9b53ea0
Krayonz <taylorkray34@gmail.com>
2025-01-26 22:22:37 -0600

    chore. cannon animation and gun addition

---
+++ bullet.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var SPEED = 100
+
+var dir : float
+var spawnPos : Vector2
+var spawnRot : float
+
+func _ready():
+	global_position = spawnPos
+	global_rotation = spawnRot
+	
+func _physics_process(_delta: float) -> void:
+	velocity = Vector2(SPEED,0).rotated(dir)
+	move_and_slide()
+	
+func _on_area_2d_body_shape_entered(_body_rid: RID, _body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
+	print("hit")
+	queue_free()
+
+func _on_life_timeout() -> void:
+	print("time out")
+	queue_free()
