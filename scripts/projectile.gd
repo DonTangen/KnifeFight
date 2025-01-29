c6c3c7f21c9c8e09f973be461e5a5cc6a01b13a7
Jacob Shirk <jacobdavidshirk@gmail.com>
2025-01-28 21:03:53 -0600

    fix: projectiles

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -1,8 +1,16 @@
 extends CharacterBody2D
 
 @export var SPEED = 80
-@onready var ray_cast: RayCast2D = $Rays/RayCast2D
-@onready var ray_cast_2: RayCast2D = $Rays/RayCast2D2
+@onready var ray_cast: RayCast2D = $LeftRays/RayCast2D
+@onready var ray_cast_2: RayCast2D = $LeftRays/RayCast2D2
+@onready var ray_cast_4: RayCast2D = $LeftRays/RayCast2D4
+@onready var ray_cast_5: RayCast2D = $LeftRays/RayCast2D5
+@onready var ray_cast_3: RayCast2D = $LeftRays/RayCast2D3
+@onready var ray_cast_6: RayCast2D = $RightRays/RayCast2D6
+@onready var ray_cast_7: RayCast2D = $RightRays/RayCast2D7
+@onready var ray_cast_8: RayCast2D = $RightRays/RayCast2D8
+@onready var ray_cast_9: RayCast2D = $RightRays/RayCast2D9
+@onready var ray_cast_10: RayCast2D = $RightRays/RayCast2D10
 
 var dir : float
 var spawnPos : Vector2
@@ -13,7 +21,7 @@ func _ready():
 	global_rotation = spawnRot
 	
 func _physics_process(_delta: float) -> void:
-	if ray_cast.is_colliding() or ray_cast_2.is_colliding():
+	if self.is_on_wall():
 		queue_free()
 	velocity = Vector2(-SPEED,0).rotated(dir)
 	move_and_slide()
