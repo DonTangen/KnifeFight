2551e0b76a8ef049b94c46fe609a84d707667938
Jacob Shirk <jacobdavidshirk@gmail.com>
2025-01-28 20:44:44 -0600

    chore: fix merge conflict

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -22,4 +22,5 @@ func _on_life_timeout() -> void:
 	queue_free()
 
 func _on_killzone_body_shape_entered(_body_rid: RID, _body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
+	print("test")
 	queue_free()
